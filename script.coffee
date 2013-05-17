2e1c302d264d5f72f4711d0010ed15ea6a7d9c2c
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-05-17 12:24:54 -0400

    vector script

--- script.coffee
+++ script.coffee
@@ -1,101 +1,3 @@
-class L.TileLayer.Ajax extends L.TileLayer
-	_requests: []
-	_data: []
-	data: ->
-		for t of @_tiles
-			tile = @_tiles[t]
-			unless tile.processed
-				@_data = @_data.concat(tile.datum)
-				tile.processed = true
-		@_data
-
-	_addTile: (tilePoint, container) ->
-		tile =
-			datum: null
-			processed: false
-
-		@_tiles[tilePoint.x + ":" + tilePoint.y] = tile
-		@_loadTile tile, tilePoint
-
-	
-	# Load the requested tile via AJAX
-	_loadTile: (tile, tilePoint) ->
-		@_adjustTilePoint tilePoint
-		layer = this
-		head = document.getElementsByTagName('head')[0]
-		cbParam = "callback"
-		cbSuffix = "_" + ("" + Math.random()).slice(2)
-		cbName = "L.TileLayer.Ajax.cb." + cbSuffix
-		scriptNode = L.DomUtil.create('script', '', head)
-		scriptNode.type = 'text/javascript'
-		L.TileLayer.Ajax.cb[cbSuffix] = (data)=>
-			head.removeChild(scriptNode)
-			delete L.TileLayer.Ajax.cb[cbSuffix]
-			tile.datum=data
-			layer._tileLoaded()
-		url = @getTileUrl(tilePoint)
-		if url.indexOf("?") == -1 
-			ourl =  url+"?"+cbParam+"="+cbName;
-		else
-			ourl =  url+"&"+cbParam+"="+cbName;
-		
-		scriptNode.src = ourl;
-		@_requests.push  {abort:()=> 
-			L.TileLayer.Ajax.cb[cbSuffix]=()->
-				head.removeChild(scriptNode)
-				delete L.TileLayer.Ajax.cb[cbSuffix]
-				false
-		}
-
-	_resetCallback: (params...)->
-		@_data = []
-		super(params...)
-		for i of @_requests
-			@_requests[i].abort()
-		@_requests = []
-
-	_update: (params...)->
-		return	if @_map._panTransition and @_map._panTransition._inProgress
-		@_tilesToLoad = 0	if @_tilesToLoad < 0
-		super params...
-L.TileLayer.Ajax.cb={}
-class L.TileLayer.GeoJSON extends L.TileLayer.Ajax
-	_geojson:
-		type: "FeatureCollection"
-		features: []
-
-	initialize: (url, options, geojsonOptions) ->
-		super url, options
-		@geojsonLayer = new L.GeoJSON(@_geojson, geojsonOptions)
-		@geojsonOptions = geojsonOptions
-
-	onAdd: (map) ->
-		@_map = map
-		super map
-		@on "load", @_tilesLoaded
-		map.addLayer @geojsonLayer
-
-	onRemove: (map) ->
-		map.removeLayer @geojsonLayer
-		@off "load", @_tilesLoaded
-		super map
-
-	data: ->
-		tileData = super()
-		for t,tileDatum of tileData
-			console.log tileDatum.features
-			@geojsonLayer.addData(tileDatum)
-		@geojsonLayer
-
-
-	_resetCallback: (params...)->
-		super params...
-
-	_tilesLoaded: (evt) ->
-		@data()
-L.tileLayer.geoJson=(params...)->
-	new L.TileLayer.GeoJSON(params...)
-
 m = L.map('map').setView([42.3453,-71.0647],16).addHash()
 
 parks = L.tileLayer.geoJson('http://{s}.tile.openstreetmap.us/vectiles-land-usages/{z}/{x}/{y}.json',{},
@@ -122,7 +24,7 @@ parks = L.tileLayer.geoJson('http://{s}.tile.openstreetmap.us/vectiles-land-usag
 			else out.fillColor = 'rgb(224,224,224)'
 		out
 ).addTo(m)
-water = L.tileLayer.geoJson('http://{s}.tile.openstreetmap.us/vectiles-water-areas/{z}/{x}/{y}.json',{},{style:
+window.water = L.tileLayer.geoJson('http://{s}.tile.openstreetmap.us/vectiles-water-areas/{z}/{x}/{y}.json',{},{style:
 	fillColor: 'rgb(151,219,242)'
 	fillOpacity:1
 	stroke:false
@@ -140,4 +42,4 @@ roads = L.tileLayer.geoJson('http://{s}.tile.openstreetmap.us/vectiles-highroad/
 		color:'rgb(240,240,240)'
 ).addTo(m)
 water.bringToFront()
-parks.sendToBack()
+parks.bringToBack()
---
+++ vectorTile.js
@@ -0,0 +1,134 @@
+L.Util.ajax = function (url,options, cb) {
+    var cbName,ourl,cbSuffix,scriptNode, head, cbParam, XMHreq;
+	if(typeof options === "function"){
+		cb = options;
+		options = {};
+	}
+	if(options.jsonp){
+		head = document.getElementsByTagName('head')[0];
+		cbParam = options.cbParam || "callback";
+		if(options.callbackName){
+			cbName= options.callbackName;
+		}else{
+			cbSuffix = "_" + ("" + Math.random()).slice(2);
+			cbName = "L.Util.ajax.cb." + cbSuffix;
+		}
+		scriptNode = L.DomUtil.create('script', '', head);
+		scriptNode.type = 'text/javascript';
+		if(cbSuffix) {
+			L.Util.ajax.cb[cbSuffix] = function(data){
+				head.removeChild(scriptNode);
+				delete L.Util.ajax.cb[cbSuffix]
+				cb(data);
+			};
+		}
+		if (url.indexOf("?") === -1 ){
+			ourl =  url+"?"+cbParam+"="+cbName;
+		}else{
+			ourl =  url+"&"+cbParam+"="+cbName;
+		}
+		scriptNode.src = ourl;
+		return {
+            abort:function(){
+    		    L.Util.ajax.cb[cbSuffix]=function(){
+                    head.removeChild(scriptNode);
+    			    delete L.Util.ajax.cb[cbSuffix];
+                    return true;
+                };
+		    }
+		};
+	}else{	
+		// the following is from JavaScript: The Definitive Guide
+		if (window.XMLHttpRequest === undefined) {
+			XMHreq = function() {
+				try {
+					return new ActiveXObject("Microsoft.XMLHTTP.6.0");
+				}
+				catch  (e1) {
+					try {
+						return new ActiveXObject("Microsoft.XMLHTTP.3.0");
+					}
+					catch (e2) {
+						throw new Error("XMLHttpRequest is not supported");
+					}
+				}
+			};
+		}else{
+			XMHreq = window.XMLHttpRequest
+		}
+		var response, request = new XMHreq();
+		request.open("GET", url);
+		request.onreadystatechange = function() {
+			if (request.readyState === 4 && request.status === 200) {
+				if(window.JSON) {
+					response = JSON.parse(request.responseText);
+				} else {
+					response = eval("("+ request.responseText + ")");
+				}
+				cb(response);
+			}
+		};
+		request.send();	
+		return request;
+	}
+};
+L.Util.ajax.cb = {};
+L.TileLayer.GeoJSON = L.TileLayer.extend({
+    _requests: [],
+    _data: [],
+    _geojson: {"type":"FeatureCollection","features":[]},
+    initialize: function (url, options, geojsonOptions) {
+        if(options.jsonp){
+            this.jsonp=true;
+        }
+        options.unloadInvisibleTiles =true;
+        L.TileLayer.prototype.initialize.call(this, url, options);
+        this.geojsonLayer = L.featureGroup([], geojsonOptions);
+        this.geojsonOptions = geojsonOptions;
+    },
+     onAdd: function (map) {
+        this._map = map;
+        L.TileLayer.prototype.onAdd.call(this, map);
+        map.addLayer(this.geojsonLayer);
+    },
+    onRemove: function (map) {
+        map.removeLayer(this.geojsonLayer);
+        L.TileLayer.prototype.onRemove.call(this, map);
+    },
+    _addTile: function(tilePoint, container) {
+        var tile = { datum: null, processed: false, id : tilePoint.x + ':' + tilePoint.y};
+        this._tiles[tile.id] = tile;
+        this._loadTile(tile, tilePoint);
+    },
+    // Load the requested tile via AJAX
+    _loadTile: function (tile, tilePoint) {
+        this._adjustTilePoint(tilePoint);
+        
+        tile._layer  = this;
+        tile.onload  = this._tileOnLoad;
+     	var _reqs = this._requests;
+        var len = _reqs.length
+        this._requests[len]=L.Util.ajax(this.getTileUrl(tilePoint),{jsonp:this.jsonp},function(data){
+            tile.datum=data;
+            tile.onload();
+            _reqs[len]=false;
+        });
+    },    
+    _update: function() {
+        if (this._map._panTransition && this._map._panTransition._inProgress) { return; }
+        if (this._tilesToLoad < 0) this._tilesToLoad = 0;
+        
+        L.TileLayer.prototype._update.apply(this, arguments);
+    },
+    _removeTile: function (id) {
+        this.geojsonLayer.removeLayer(this._tiles[id]._jsonLayer);
+        delete this._tiles[id];
+    },
+    _tileOnLoad: function (e) {
+    		this._jsonLayer = L.geoJson(this.datum,this._layer.geojsonOptions);
+    		this._layer.geojsonLayer.addLayer(this._jsonLayer);
+    }
+});
+L.tileLayer.geoJson=function(a,b,c){
+    return new L.TileLayer.GeoJSON(a,b,c);
+}
